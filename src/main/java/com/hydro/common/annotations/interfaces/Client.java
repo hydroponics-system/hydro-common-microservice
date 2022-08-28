92e349eb0663a6ec6d4e449e40d181783f2f5fc4
Samuel Butler <sambutler1017@icloud.com>
2022-08-27 22:28:43 -0400

    no message

---
+++ src/main/java/com/hydro/common/annotations/interfaces/Client.java
@@ -0,0 +1,16 @@
+package com.hydro.common.annotations.interfaces;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Default annotation for client classes.
+ * 
+ * @author Sam Butler
+ * @since July 31, 2021
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface Client {}
