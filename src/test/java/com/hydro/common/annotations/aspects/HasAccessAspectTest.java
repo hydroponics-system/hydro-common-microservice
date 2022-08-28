cc73a1d4c661ee9e813d71e25e053abdf7dacade
Samuel Butler <sambutler1017@icloud.com>
2022-08-27 23:14:47 -0400

    no message

---
+++ src/test/java/com/hydro/common/annotations/aspects/HasAccessAspectTest.java
@@ -0,0 +1,80 @@
+package com.hydro.common.annotations.aspects;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.lang.annotation.Annotation;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.hydro.common.annotations.interfaces.HasAccess;
+import com.hydro.common.dictionary.enums.WebRole;
+import com.hydro.common.exception.exceptions.InsufficientPermissionsException;
+import com.hydro.common.jwt.utility.JwtHolder;
+import com.hydro.test.factory.annotations.HydroTest;
+
+/**
+ * Test class for the HasAccess Aspect.
+ * 
+ * @author Sam Butler
+ * @since August 23, 2022
+ */
+@HydroTest
+@ExtendWith(MockitoExtension.class)
+public class HasAccessAspectTest {
+    @Mock
+    private ProceedingJoinPoint proceedingJoinPoint;
+
+    @Mock
+    private JwtHolder jwtHolder;
+
+    @InjectMocks
+    private HasAccessAspect accessAspcect;
+
+    @Test
+    public void testHasAccessAdminAccess() throws Throwable {
+        when(jwtHolder.getWebRole()).thenReturn(WebRole.ADMIN);
+        accessAspcect.access(proceedingJoinPoint, getHasAccessAnnotationInstance(WebRole.ADMIN));
+        verify(proceedingJoinPoint).proceed();
+    }
+
+    @ParameterizedTest
+    @EnumSource(WebRole.class)
+    public void testHasAccessAllAccess(WebRole role) throws Throwable {
+        when(jwtHolder.getWebRole()).thenReturn(role);
+        accessAspcect.access(proceedingJoinPoint, getHasAccessAnnotationInstance(WebRole.USER));
+        verify(proceedingJoinPoint).proceed();
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = WebRole.class, names = { "ADMIN" }, mode = EnumSource.Mode.EXCLUDE)
+    public void testHasAccessInsufficentPermissions(WebRole role) throws Throwable {
+        when(jwtHolder.getWebRole()).thenReturn(role);
+        assertThrows(InsufficientPermissionsException.class,
+                     () -> accessAspcect.access(proceedingJoinPoint, getHasAccessAnnotationInstance(WebRole.ADMIN)));
+        verify(proceedingJoinPoint, never()).proceed();
+    }
+
+    private HasAccess getHasAccessAnnotationInstance(WebRole role) {
+        return new HasAccess() {
+            @Override
+            public WebRole value() {
+                return role;
+            }
+
+            @Override
+            public Class<? extends Annotation> annotationType() {
+                return HasAccess.class;
+            }
+        };
+    }
+}
---
+++ src/test/java/com/hydro/common/datetime/DateTimeMapperTest.java
@@ -0,0 +1,43 @@
+package com.hydro.common.datetime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+import com.hydro.test.factory.annotations.HydroTest;
+
+/**
+ * Test class for the Date Time Mapper.
+ * 
+ * @author Sam Butler
+ * @since August 23, 2022
+ */
+@HydroTest
+public class DateTimeMapperTest {
+    @Test
+    public void testFormatDateGivenFormat() {
+        Date testDate = new Date(100000);
+        assertEquals("01-01-1970", DateTimeMapper.printDate(testDate, "MM-dd-yyyy"));
+    }
+
+    @Test
+    public void testPrintDateDefaultFormat() {
+        Date testDate = new Date(100000);
+        assertEquals("1970-01-01 00:01:40", DateTimeMapper.printDate(testDate));
+    }
+
+    @Test
+    public void testFormatLocalDateShouldFormatDatetoString() {
+        LocalDateTime testDate = LocalDateTime.of(2012, 2, 12, 10, 22, 15);
+        assertEquals("02-12-2012 10:22:15", DateTimeMapper.printDate(testDate, "MM-dd-yyyy HH:mm:ss"));
+    }
+
+    @Test
+    public void testPrintDateDefaultSqlFormat() {
+        LocalDateTime testDate = LocalDateTime.of(2012, 2, 12, 10, 22, 15);
+        assertEquals("2012-02-12 10:22:15", DateTimeMapper.printDate(testDate));
+    }
+}
